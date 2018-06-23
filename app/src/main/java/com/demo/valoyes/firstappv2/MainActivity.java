53ab60978bd7bcc9ff6ff0dba3bfde8b40787eba
benito <jlbeny100@gmail.com>
2018-06-23 21:32:34 +0200

    add Worker class and some code

--- app/src/main/java/com/demo/valoyes/firstappv2/MainActivity.java
+++ app/src/main/java/com/demo/valoyes/firstappv2/MainActivity.java
@@ -8,21 +8,32 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        // establece cual sera el layout para esta activity
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+
+        // codigo que se va a ejecutar cuando el usuario hace click en el fab button
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+
+                TextView textValue = (TextView) findViewById(R.id.text_value);
+                String valorOriginal = textValue.getText().toString();
+                int doubledValue = Integer.parseInt(valorOriginal)*2;
+                textValue.setText(String.valueOf(doubledValue));
+
+                Snackbar.make(view, "Valor original " + valorOriginal + " x 2 es igual a " + doubledValue
+                        , Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
         });
---
+++ app/src/main/java/com/demo/valoyes/firstappv2/MyWorker.java
@@ -0,0 +1,9 @@
+package com.demo.valoyes.firstappv2;
+
+public class MyWorker {
+
+    public static int doubleTheValue(int value){
+        return value*2;
+
+    }
+}
